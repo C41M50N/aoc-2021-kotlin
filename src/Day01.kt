bdd7d3ab1c2df1eafd0ef0fff81c143dbc174047
Charles Buffington II <chuckinator2020@gmail.com>
2021-12-02 08:43:39 -0500

    complete day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,66 @@
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(depths: List<Int>): Int {
+        var numDepthsIncreased = 0
+
+        val depthsIterator = depths.iterator()
+        var previous = depthsIterator.next()
+        while (depthsIterator.hasNext()) {
+            val current = depthsIterator.next()
+
+            if (current > previous) numDepthsIncreased++
+
+            previous = current
+        }
+
+        return numDepthsIncreased
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1UsingWindowed(depths: List<Int>): Int {
+        return depths.windowed(2).count { (previous, current) -> current > previous }
+    }
+
+    fun part2(depths: List<Int>): Int {
+        var numDepthsIncreased = 0
+
+        var startIndex = 0
+        var endIndex = 2
+
+        var previous = depths.getSumOfRange(startIndex..endIndex)
+        while (endIndex + 1 < depths.size) {
+            val current = depths.getSumOfRange(++startIndex..++endIndex)
+
+            if (current > previous) numDepthsIncreased++
+
+            previous = current
+        }
+
+        return numDepthsIncreased
+    }
+
+    fun part2UsingWindowed(depths: List<Int>): Int {
+        return depths.windowed(4).count {
+            (a,b,c,d) ->
+                run {
+                    val previousSum = a + b + c
+                    val currentSum = b + c + d
+                    currentSum > previousSum
+                }
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputInt("Day01_test")
+    println(part1(testInput) == 7)
+    check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInputInt("Day01")
     println(part1(input))
+    println(part1UsingWindowed(input))   // 1298
     println(part2(input))
+    println(part2UsingWindowed(input))   // 1248
+}
+
+private fun List<Int>.getSumOfRange(range: IntRange): Int {
+    return this.slice(range).sum()
 }
